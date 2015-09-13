b2d382a100f4be451798ce53ff8ab2fe8a66dcfd
efimweb <efimweb@yandex.ru>
2015-09-14 00:18:15 +0300

    add mongo

--- login.js
+++ login.js
@@ -1,14 +1,17 @@
 var express = require('express'),
     passport = require('passport'),
-    util = require('util'),
     GitHubStrategy = require('passport-github').Strategy,
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
-    session = require('express-session');
+    session = require('express-session'),
+    MongoClient = require('mongodb').MongoClient,
+    assert = require('assert'),
+    MongoStore = require('connect-mongo')(session);
 
 var GITHUB_CLIENT_ID = "ceb3d160e51558c5467e"
 var GITHUB_CLIENT_SECRET = "b67e9d131aab9747c55fdeaa0776b69945c6b1de";
+var mongoUrl = 'mongodb://localhost:27017/users';
 
 
 // Passport session setup.
@@ -44,7 +47,17 @@ passport.use(new GitHubStrategy({
             // represent the logged-in user.  In a typical application, you would want
             // to associate the GitHub account with a user record in your database,
             // and return that user instead.
-            return done(null, accessToken);
+            var user = {
+                userName: profile.username,
+                displayName: profile.displayName,
+                email: profile.emails[0].value,
+                profileUrl: profile.profileUrl,
+                avatarUrl: profile._json.avatar_url,
+                githubId: profile.id,
+                accessToken: accessToken
+            }
+            return done(null, user);
+
         });
     }
 ));
@@ -61,7 +74,7 @@ app.set('view engine', 'ejs');
 app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(bodyParser.json());
 
@@ -69,8 +82,18 @@ app.use(methodOverride());
 app.use(session({
     secret: 'keyboard cat',
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
+    store: new MongoStore({
+        url: mongoUrl
+    })
 }));
+
+
+
+//app.use(function(req, res, next){
+//    req.session.numberOfVisits = req.session.numberOfVisits + 1 || 1;
+//    res.send("Visits" + req.session.numberOfVisits)
+//});
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
 app.use(passport.initialize());
@@ -79,13 +102,10 @@ app.get('/', function (req, res) {
     res.render('index', {
         user: req.user
     });
+
 });
 
-app.get('/account', ensureAuthenticated, function (req, res) {
-    res.render('account', {
-        user: req.user
-    });
-});
+
 
 app.get('/login', function (req, res) {
     res.render('login', {
@@ -116,7 +136,39 @@ app.get('/auth/github/callback',
     }),
     function (req, res) {
         res.redirect('/');
+
+
+        var insertUser = function (db, callback) {
+            // Get the documents collection
+            var collection = db.collection('user');
+            // Insert some documents
+            collection.ensureIndex({
+                gitId: 1
+            }, {
+                unique: true
+            });
+            collection.insert({
+                userName: req.user.userName,
+                displayName: req.user.displayName,
+                email: req.user.email,
+                profileUrl: req.user.profileUrl,
+                avatarUrl: req.user.avatarUrl,
+                githubId: req.user.githubId,
+                accessToken: req.user.accessToken
+            });
+
+        }
+
+        MongoClient.connect(mongoUrl, function (err, db) {
+            assert.equal(null, err);
+            console.log("Connected correctly to server");
+            insertUser(db, function () {
+                db.close();
+            });
+        });
     });
+
+
 
 app.get('/logout', function (req, res) {
     req.logout();
@@ -127,12 +179,13 @@ app.use(express.static(__dirname + '/public'));
 
 
 
+
 var server = app.listen(3000, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+    var host = server.address().address
+    var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
+    console.log('Example app listening at http://%s:%s', host, port)
 
 })
 
